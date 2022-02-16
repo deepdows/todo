24be27bceba0107f7b521571d0c8323c45b67fbb
Dmytro <deepdows@gmail.com>
2022-02-16 18:56:07 +0200

    fixinf database

--- backend/database.py
+++ backend/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-#SQLALCHEMY_DATABASE_URL = "postgresql://user:secret@todo-db:5432/db"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://v8Wef6QCGp:65pYdU5Qnp@remotemysql.com:3306/v8Wef6QCGp"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:secret@todo-db:5432/db"
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://v8Wef6QCGp:65pYdU5Qnp@remotemysql.com:3306/v8Wef6QCGp"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
